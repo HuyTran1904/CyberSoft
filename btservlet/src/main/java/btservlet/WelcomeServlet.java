f910042a05936277ae33ecfd999fc3710b8042f6
HuyTran1904 <huytran98447@gmail.com>
2025-01-20 23:59:02 +0700

    Add files via upload

---
+++ btservlet/src/main/java/btservlet/WelcomeServlet.java
@@ -0,0 +1,17 @@
+package btservlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h2>Chào mừng, admin!</h2>");
+        response.getWriter().println("<a href='login.jsp'>Đăng xuất</a>");
+    }
+}
